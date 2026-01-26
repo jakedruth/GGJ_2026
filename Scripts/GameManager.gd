f627b541ead10130d6b5dac141fb660cf2f3e85d
jakedruth <jakedruth@gmail.com>
2026-01-26 18:16:19 -0500

    Messing Around with the markdown in IDEs

--- Scripts/GameManager.gd
+++ Scripts/GameManager.gd
@@ -3,9 +3,14 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	_test_func()
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	pass
+
+## Test Fucntion **Bold Test**
+func _test_func() -> void:
 	pass
