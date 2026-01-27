443287e66da02797af6c6df3c2b6db35c53e6534
jakedruth <jakedruth@gmail.com>
2026-01-26 22:02:39 -0500

    Got the ratio of overlap for percentages

--- Scripts/GameManager.gd
+++ Scripts/GameManager.gd
@@ -1,7 +1,7 @@
 extends Node
 
-@export var characterStats: Resource;
-@export var questStats: Resource;
+@export var characterStats: Stats;
+@export var questStats: Stats;
 @export var polygon2D: Polygon2D;
 
 var isChar: bool = false;
@@ -9,8 +9,12 @@ var isChar: bool = false;
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	var poly = characterStats.get_polygon();
+	var poly = characterStats.to_polygon2D();
 	polygon2D.polygon = poly;
+
+	var result = characterStats.get_overlap_area_ratio(questStats)
+	result = questStats.get_overlap_area_ratio(characterStats)
+	print(result);
 
 	pass # Replace with function body.
 
@@ -24,6 +28,6 @@ func _input(event: InputEvent) -> void:
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
 		isChar = !isChar
 		if isChar:
-			polygon2D.polygon = characterStats.get_polygon();
+			polygon2D.polygon = characterStats.to_polygon2D();
 		else:
-			polygon2D.polygon = questStats.get_polygon();
+			polygon2D.polygon = questStats.to_polygon2D();
--- Scripts/Stats.gd
+++ Scripts/Stats.gd
@@ -6,30 +6,51 @@ class_name Stats extends Resource
 @export var CHA: int = 0
 @export var INT: int = 0
 
-
 func get_overlap_area_ratio(other: Stats) -> float:
-	var poly = get_polygon()
-	var _otherPoly = other.get_polygon()
+	var _a = to_polygon2D();
+	var _b = other.to_polygon2D();
+	var _overlap = Geometry2D.intersect_polygons(_a, _b)[0];
 
-	for p in poly:
-		print("point:", p.x, "\t", p.y)
-		pass
+	var _areaA = _get_area(_a);
+	var _areaB = _get_area(_b);
+	var _areaO = _get_area(_overlap);
 
-	return other.POW;
+	var ratio = _areaO / _areaB
+
+	return ratio;
+
+
+func get_overlap_with_other(other: Stats) -> PackedVector2Array:
+	var _a = to_polygon2D()
+	var _b = other.to_polygon2D()
+	var overlap = Geometry2D.intersect_polygons(_a, _b);
+	return overlap
 
  
-func get_polygon() -> Array[Vector2]:
-	const delta = 2 * PI * 0.2;
-	var arr: Array[Vector2] = [
+func to_polygon2D() -> PackedVector2Array:
+	const delta = 2 * PI * 0.2
+	var verts: PackedVector2Array = [
 		_get_point(POW, 0 * delta),
 		_get_point(SNK, 1 * delta),
 		_get_point(SPD, 2 * delta),
 		_get_point(CHA, 3 * delta),
-		_get_point(INT, 4 * delta),
-	];
-	return arr
+		_get_point(INT, 4 * delta)];
+	return verts
 
 
 ## Converts a radial coord to cartesian coord
 func _get_point(radius: int, angle: float) -> Vector2:
 	return Vector2(cos(angle), sin(angle)) * radius
+
+func _get_area(verts: PackedVector2Array) -> float:
+	var area = 0;
+	var productA = 0;
+	var productB = 0;
+	for i in verts.size():
+		var n = (i + 1) % verts.size();
+		productA += verts[i].x * verts[n].y
+		productB += verts[i].y * verts[n].x
+		pass
+	area = (productA - productB) * 0.5
+	
+	return area;
