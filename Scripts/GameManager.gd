ce6921b204a96bcf6df29f279b6611fb162e3497
jakedruth <jakedruth@gmail.com>
2026-01-29 18:20:28 -0500

    Added in the ball bouncing inside of a polygon

--- Scripts/GameManager.gd
+++ Scripts/GameManager.gd
@@ -9,13 +9,20 @@ extends Node
 var _jitterTimerTeam: float = 0;
 var _jitterTimerQuest: float = 0;
 
-var isChar: bool = false;
+var startBallMove: bool = false;
+var startDrag: bool = false;
+var ball: Node2D;
+var ball_move_dir: Vector2 = Vector2.ZERO
+var ball_speed: float = 0
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	_set_polygon(questPoly, questStats.to_polygon2D());
-	_set_polygon(teamPoly, characterStats.to_polygon2D());
+	_set_polygon(questPoly, questStats._to_packedVector2Array());
+	_set_polygon(teamPoly, characterStats._to_packedVector2Array());
+
+	ball = get_node("TextureRect/center/Sprite2D") as Node2D
+	_reset_ball()
 
 	var result = characterStats.get_overlap_area_ratio(questStats)
 	print(result);
@@ -30,21 +37,101 @@ func _process(_delta: float) -> void:
 	
 	if (_jitterTimerQuest > jitterRate):
 		_jitterTimerQuest -= jitterRate + randf_range(0, jitterRange)
-		var qp: PackedVector2Array = questStats.to_polygon2D();
+		var qp: PackedVector2Array = questStats.get_polygon();
 		for i in qp.size():
 			var angle = randf_range(0, TAU)
 			qp[i] += Vector2.RIGHT.rotated(angle) * 0.05
 		_set_polygon(questPoly, qp)
 	if (_jitterTimerTeam > jitterRate):
 		_jitterTimerTeam -= jitterRate + randf_range(0, jitterRange)
-		var tp: PackedVector2Array = characterStats.to_polygon2D();
+		var tp: PackedVector2Array = characterStats.get_polygon();
 		for i in tp.size():
 			var angle = randf_range(0, TAU)
 			tp[i] += Vector2.RIGHT.rotated(angle) * 0.05
 		_set_polygon(teamPoly, tp)
+
+	if startBallMove:
+		var poly = questStats.get_polygon();
+
+		if not Geometry2D.is_point_in_polygon(ball.position, poly):
+			var center = questStats.get_center();
+			ball_move_dir = (center - ball.position).normalized()
+			pass
+
+		if startDrag:
+			ball_speed = lerp(ball_speed, 0.0, 1.0 - exp(-0.75 * _delta))
+			pass
+
+		var step = ball_speed * _delta;
+		var next = ball.position + ball_move_dir * step;
+		# check to see if the next point is outside the polygon
+
+		if not Geometry2D.is_point_in_polygon(next, poly):
+			# Keep finding intersection until there are none
+			var seg_flag = -1;
+			var pos = ball.position;
+			while step > 0:
+				for i in poly.size():
+					if seg_flag == i:
+						continue
+
+					var s = poly[i];
+					var e = poly[(i + 1)%poly.size()]
+					var intersection = Geometry2D.segment_intersects_segment(pos, next, s, e)
+					if (intersection != null):
+						print("checking collision")
+						seg_flag = i;
+						var segmentDirection = (e - s).normalized()
+						var segmentNormal = Vector2(-segmentDirection.y, segmentDirection.x).normalized()
+						var wall_distance = ball.position.distance_to(intersection)
+						step -= wall_distance
+						ball_move_dir = ball_move_dir.bounce(segmentNormal).normalized()
+						pos = intersection
+						next = intersection + ball_move_dir * step
+						i = 0
+					pass
+				print("Checked all segments")
+				break
+
+		ball.position = next;
 
 	pass
 
 func _set_polygon(polygon: Polygon2D, points: PackedVector2Array) -> void:
 	polygon.polygon = points;
 	(polygon.get_child(0) as Line2D).points = points;
+	for i in points.size():
+		(polygon.get_child(i + 1) as Node2D).position = points[i];
+		pass
+
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		if event.button_index == 1: # Left Click
+			_handle_start_ball()
+			pass
+		elif event.button_index == 2: # Right Click
+			_reset_ball()
+			pass
+		elif event.button_index == 3: # Middle Click
+			startDrag = true
+			pass
+		
+	pass
+
+func _reset_ball() -> void:
+	startBallMove = false;
+	startDrag = false;
+	ball.position = questStats.get_center();
+	ball_move_dir = Vector2.ZERO
+	pass
+
+func _handle_start_ball() -> void:
+	if startBallMove == true:
+		return
+	
+	startBallMove = true;
+	var angle = randf_range(0, TAU)
+	ball_move_dir = Vector2.RIGHT.rotated(angle);
+	ball_speed = 20;
+	pass
--- Scripts/Stats.gd
+++ Scripts/Stats.gd
@@ -16,10 +16,12 @@ enum Stat {
 	Stat.INT: 0
 }
 
+var _polygon: PackedVector2Array;
+
 
 func get_overlap_area_ratio(other: Stats) -> float:
-	var _a = to_polygon2D();
-	var _b = other.to_polygon2D();
+	var _a = _to_packedVector2Array();
+	var _b = other._to_packedVector2Array();
 	var _overlap = Geometry2D.intersect_polygons(_a, _b)[0];
 
 	var _areaA = _get_area(_a);
@@ -32,21 +34,39 @@ func get_overlap_area_ratio(other: Stats) -> float:
 
 
 func get_overlap_with_other(other: Stats) -> PackedVector2Array:
-	var _a = to_polygon2D()
-	var _b = other.to_polygon2D()
+	var _a = _to_packedVector2Array()
+	var _b = other._to_packedVector2Array()
 	var overlap = Geometry2D.intersect_polygons(_a, _b);
 	return overlap
 
+func get_polygon() -> PackedVector2Array:
+	if _polygon.size() == 0:
+		_polygon = _to_packedVector2Array()
+
+	return _polygon.slice(0, _polygon.size())
+
  
-func to_polygon2D() -> PackedVector2Array:
+func _to_packedVector2Array() -> PackedVector2Array:
 	const delta = 2 * PI * 0.2
-	var verts: PackedVector2Array = [
+	var points: PackedVector2Array = [
 		_get_point(data[Stat.POW], 0 * delta),
 		_get_point(data[Stat.SNK], 1 * delta),
 		_get_point(data[Stat.SPD], 2 * delta),
 		_get_point(data[Stat.CHA], 3 * delta),
 		_get_point(data[Stat.INT], 4 * delta)];
-	return verts
+	return points
+
+func get_center() -> Vector2:
+	var points = _to_packedVector2Array()
+	var center = Vector2.ZERO
+	var size: float = points.size();
+	if points.size() == 0:
+		return center;
+
+	for point in points:
+		center += point / size;
+
+	return center;
 
 
 ## Converts a radial coord to cartesian coord
@@ -54,14 +74,14 @@ func _get_point(radius: int, angle: float) -> Vector2:
 	return Vector2(cos(angle), sin(angle)) * radius
 
 
-func _get_area(verts: PackedVector2Array) -> float:
+func _get_area(points: PackedVector2Array) -> float:
 	var area = 0;
 	var productA = 0;
 	var productB = 0;
-	for i in verts.size():
-		var n = (i + 1) % verts.size();
-		productA += verts[i].x * verts[n].y
-		productB += verts[i].y * verts[n].x
+	for i in points.size():
+		var n = (i + 1) % points.size();
+		productA += points[i].x * points[n].y
+		productB += points[i].y * points[n].x
 		pass
 	area = (productA - productB) * 0.5
 	
